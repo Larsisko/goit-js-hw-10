179d4c3ba19cf58f7a25bae04f5b8f2a7ac145b8
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-10-12 11:00:02 +0200

    function repaired

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
 import { fetchBreedsList, fetchCatByBreed } from './partials/cat-api';
-
+document.querySelector('.error').style.display = 'none';
 const breedSelect = document.querySelector('.breed-select');
 
-breedSelect.addEventListener('click', () => {
-  fetchBreedsList();
-});
-document.querySelector('.loader').style.display = 'none';
-document.querySelector('.error').style.display = 'none';
+// breedSelect.addEventListener('click', () => {
+//   fetchBreedsList();
+// });
+const loadData = () => {
+  document.querySelector('.loader').style.display = 'block';
+  fetchBreedsList()
+    .then(data => {
+      const select = document.querySelector('.breed-select');
+      data.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        select.appendChild(option);
+      });
+      document.querySelector('.loader').style.display = 'none';
+    })
+    .catch(error => {
+      const errorElement = document.querySelector('.error');
+      errorElement.style.display = 'block';
+      console.error('There was a problem with the fetch operation:', error);
+    });
+  document.querySelector('.loader').style.display = 'none';
+};
 
 breedSelect.addEventListener('change', event => {
   const selectedBreedId = event.target.value;
   console.log(selectedBreedId);
-  fetchCatByBreed(selectedBreedId);
+  document.querySelector('.loader').style.display = 'block';
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      const catData = data[0];
+      const catName = selectedBreedId;
+      const catDescription = catData.breeds[0].description;
+      const catTemperament = catData.breeds[0].temperament;
+
+      const catInfoElement = document.querySelector('.cat-info');
+      const loaderElement = document.querySelector('.loader');
+      loaderElement.style.display = 'none';
+      catInfoElement.style.display = 'block';
+      catInfoElement.innerHTML = `<div class='catContainer'>
+        <img class="cat_img" src="${catData.url}" alt="${catName}"  />
+        <h2>${catName}</h2>
+        <p><strong>Description:</strong> ${catDescription}</p>
+        <p><strong>Temperament:</strong> ${catTemperament}</p>
+      </div>`;
+    })
+    .catch(error => {
+      document.querySelector('.error').style.display = 'block';
+      console.error('There was a problem with the fetch operation:', error);
+      document.querySelector('.loader').style.display = 'none';
+    });
 });
+document.addEventListener('DOMContentLoaded', loadData);
--- src/partials/cat-api.js
+++ src/partials/cat-api.js
@@ -4,10 +4,9 @@ const apiKey =
   'live_dQsNZ0DNtQU2fFVBQ3mGqOSNuREhMUluEfZX2n00H9RCJqh0J1ZiFFhM7B1wS72f';
 axios.defaults.headers.common['x-api-key'] = apiKey;
 const url = 'https://api.thecatapi.com/v1/breeds';
-function fetchBreedsList() {
-  document.querySelector('.loader').style.display = 'block';
 
-  fetch(url, {
+export function fetchBreedsList() {
+  return fetch(url, {
     method: 'GET',
     headers: {
       'x-api-key': apiKey,
@@ -15,69 +14,32 @@ function fetchBreedsList() {
   })
     .then(response => {
       if (!response.ok) {
-        document.querySelector('.error').style.display = 'block';
+        throw new Error('There was a problem with the fetch operation');
       }
-      document.querySelector('.loader').style.display = 'none';
       return response.json();
     })
-    .then(data => {
-      const select = document.querySelector('.breed-select');
-      data.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        select.appendChild(option);
-      });
-    })
     .catch(error => {
-      document.querySelector('.loader').style.display = 'none';
-
-      console.error('There was a problem with the fetch operation:', error);
+      throw error;
     });
 }
-
-function fetchCatByBreed(breedId) {
-  const catInfoElement = document.querySelector('.cat-info');
-  const loaderElement = document.querySelector('.loader');
+export function fetchCatByBreed(breedId) {
+  // const catInfoElement = document.querySelector('.cat-info');
+  // const loaderElement = document.querySelector('.loader');
   const catInfoUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-  catInfoElement.style.display = 'none';
-  loaderElement.style.display = 'block';
 
   return fetch(catInfoUrl, {
     method: 'GET',
     headers: {
-      'x-api-key':
-        'live_dQsNZ0DNtQU2fFVBQ3mGqOSNuREhMUluEfZX2n00H9RCJqh0J1ZiFFhM7B1wS72f',
+      'x-api-key': apiKey,
     },
   })
     .then(response => {
       if (!response.ok) {
-        document.querySelector('.error').style.display = 'block';
+        throw new Error('There was a problem with the fetch operation');
       }
       return response.json();
     })
-    .then(data => {
-      loaderElement.style.display = 'none';
-      catInfoElement.style.display = 'block';
-
-      const catData = data[0];
-      const catName = breedId;
-      const catDescription = catData.breeds[0].description;
-      const catTemperament = catData.breeds[0].temperament;
-
-      catInfoElement.innerHTML = `<div class='catContainer'>
-        <img clas ="cat_img" src="${catData.url}" alt="${catName}"  />
-        <h2>${catName}</h2>
-        <p><strong>Description:</strong> ${catDescription}</p>
-        <p><strong>Temperament:</strong> ${catTemperament}</p>
-      </div>`;
-    })
-
     .catch(error => {
-      document.querySelector('.error').style.display = 'block';
-      console.error('There was a problem with the fetch operation:', error);
+      throw error;
     });
 }
-
-export { fetchBreedsList, fetchCatByBreed };
